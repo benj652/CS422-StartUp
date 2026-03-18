9898b9f8ccabfcea475c88d30ffc42ccb3a327e5
benj652 <benjaminjaffe8@gmail.com>
2026-03-18 14:09:50 -0400

    user is reserved name in postgress it was causing a crash

--- website/models/tracking.py
+++ website/models/tracking.py
@@ -2,6 +2,7 @@ from website import db
 from datetime import datetime
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     # This stores the UUID for anonymous tracking until they "sign up" or identify
     uuid = db.Column(db.String(100), unique=True, nullable=False)
